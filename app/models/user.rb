68b9b27fff5d55cdc14a304707861fcc3836df9c
Haris Siddique <faaisalsaddiq@gmail.com>
2024-09-03 15:57:20 +0500

    miner changes in frount end

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,7 @@ class User < ApplicationRecord
   has_many :projects_assigneds, dependent: :destroy
   has_many :assigned_projects, through: :projects_assigneds, source: :project
 
-  has_many :bugs_as_developer, class_name: 'Bug', foreign_key: 'developer_id'
-  has_many :bugs_as_qa, class_name: 'Bug', foreign_key: 'qa_id', dependent: :destroy
-  has_many :bugs_as_manager, class_name: 'Bug', foreign_key: 'manager_id', dependent: :destroy
-
- 
-
-
-  def manager?
-    user_type == 'manager'
-  end
-
-  def developer?
-    user_type == 'developer'
-  end
-
-  def qa?
-    user_type == 'qa'
-  end
+  # has_many :bugs_as_developer, class_name: 'Bug', foreign_key: 'developer_id'
+  # has_many :bugs_as_qa, class_name: 'Bug', foreign_key: 'qa_id', dependent: :destroy
+  # has_many :bugs_as_manager, class_name: 'Bug', foreign_key: 'manager_id', dependent: :destroy
 end
